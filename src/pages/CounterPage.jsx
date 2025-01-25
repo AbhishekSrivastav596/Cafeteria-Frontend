d56d69053d7299dc9a8dee617b1fe3e13abe42c1
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-01-25 15:03:45 +0530

    added carousel

---
+++ src/pages/CounterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CounterPage() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CounterPage
---
+++ src/pages/DishPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DishPage() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default DishPage
