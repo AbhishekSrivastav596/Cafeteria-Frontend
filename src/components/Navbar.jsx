51f6d3c5f067c7bd3310442cb74bf83595cb6570
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-02-05 18:48:21 +0530

    fixed buttons positioning in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,18 +10,24 @@ import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import settingIcon from "../assets/settingIcon.png";
 import cartIcon from "../assets/cart.png";
-import { useEffect } from "react";
+import Tooltip from "@mui/material/Tooltip";
+import HomeIcon from "@mui/icons-material/Home";
 
 const pages = ["Dishes", "Counters"];
-const settings = ["Profile", "Logout"];
+const settings = [
+  { name: "Profile", path: "/profile" },
+  { name: "Login", path: "/login" },
+  { name: "Signup", path: "/signup" },
+  { name: "Logout", path: "#" },
+];
 
 function Navbar() {
-  const totalQuantity = useSelector((state) => state.cart.cartItems.reduce((acc, item) => acc + item.quantity, 0));
-  const loading = useSelector(state => state.cart.loading);
+  const totalQuantity = useSelector((state) =>
+    state.cart.cartItems.reduce((acc, item) => acc + item.quantity, 0)
+  );
+  const loading = useSelector((state) => state.cart.loading);
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -45,41 +51,30 @@ function Navbar() {
     <AppBar position="sticky" sx={{ backgroundColor: "#eed9c4" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
+          <Box sx={{ flexGrow: 1 }}>
+            <Tooltip title="Open Menu">
+              <IconButton onClick={handleOpenUserMenu} sx={{ color: "black" }}>
+                <MenuIcon />
+              </IconButton>
+            </Tooltip>
             <Menu
+              sx={{ mt: "45px" }}
               id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
+              anchorEl={anchorElUser}
+              anchorOrigin={{ vertical: "top", horizontal: "left" }}
               keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
+              {settings.map((setting) => (
+                <MenuItem key={setting.name} onClick={handleCloseUserMenu}>
                   <Typography sx={{ textAlign: "center", color: "black" }}>
                     <Link
-                      to={`/${page.toLowerCase()}`}
+                      to={setting.path}
                       style={{ textDecoration: "none", color: "black" }}
                     >
-                      {page}
+                      {setting.name}
                     </Link>
                   </Typography>
                 </MenuItem>
@@ -87,12 +82,16 @@ function Navbar() {
             </Menu>
           </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Link to="/" style={{ color: "black" }}>
+              <IconButton sx={{ color: "black" }}>
+                <HomeIcon />
+              </IconButton>
+            </Link>
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "black", display: "block" }}
+                sx={{ my: 2, mr: 1, color: "black", display: "block" }}
               >
                 <Link
                   to={`/${page.toLowerCase()}`}
@@ -102,76 +101,47 @@ function Navbar() {
                 </Link>
               </Button>
             ))}
-          </Box>
-          <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
-            
-          <Link
-                to="/cart"
-                style={{ textDecoration: "none", color: "black" }}
+            <Link to="/cart" style={{ textDecoration: "none", color: "black" }}>
+              <Button
+                sx={{
+                  color: "black",
+                  textTransform: "none",
+                  mr: 2,
+                  display: "flex",
+                  alignItems: "center",
+                }}
               >
-            <Button
-              sx={{
-                color: "black",
-                textTransform: "none",
-                mr: 2,
-                display: "flex",
-                alignItems: "center",
-              }}
-            >   
-             {loading ? ("Loading...") : (
-                <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
-                  <div style={{ position: "relative" }}>
-                    <img
-                      src={cartIcon}
-                      alt="Cart"
-                      style={{ width: "30px", height: "30px", marginRight: "3px" }}
-                    />
-                    {totalQuantity > 0 && (
-                      <span className="absolute -top-3 -right-1 bg-orange-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                        {totalQuantity}
-                      </span>
-                    )}
+                {loading ? (
+                  "Loading..."
+                ) : (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      position: "relative",
+                    }}
+                  >
+                    <div style={{ position: "relative" }}>
+                      <img
+                        src={cartIcon}
+                        alt="Cart"
+                        style={{
+                          width: "30px",
+                          height: "30px",
+                          marginRight: "3px",
+                        }}
+                      />
+                      {totalQuantity > 0 && (
+                        <span className="absolute -top-3 -right-1 bg-orange-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                          {totalQuantity}
+                        </span>
+                      )}
+                    </div>
+                    <span style={{ marginLeft: "3px" }}>Cart</span>
                   </div>
-                  <span style={{ marginLeft: "5px" }}>Cart</span>
-                </div>
-              )}
-            </Button>
+                )}
+              </Button>
             </Link>
-
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <img
-                  src={settingIcon}
-                  alt="Settings"
-                  style={{ width: "25px", height: "25px", cursor: "pointer" }}
-                />
-              </IconButton>
-            </Tooltip>
-
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center", color: "black" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
